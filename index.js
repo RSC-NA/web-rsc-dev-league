b3505e3669bc1817f36999af03fe1d11f86bc26f
Eric Harrison <me@ericharrison.info>
2023-06-22 18:17:34 -0400

    Install package

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const cors = require('cors');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 
