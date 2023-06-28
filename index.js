b6b731505566eb37867d474b566dbb660727ff2f
Eric Harrison <me@ericharrison.info>
2023-06-28 10:39:33 -0400

    bumps version to 2.3.0

--- index.js
+++ index.js
@@ -568,7 +568,7 @@ app.get('/matches', (req, res) => {
  ****************** TRACKER/MMR TOOL ********************
  *******************************************************/
 
-const EXTENSION_VERSION = '2.2.2';
+const EXTENSION_VERSION = '2.3.0';
 const tracker_queue = {};
 app.get('/get_tracker', async (req, res) => {
 	let len = Object.keys(tracker_queue).length;
