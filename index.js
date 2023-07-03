ca285dea938963312be04294d06c5ef9030bce83
Eric Harrison <me@ericharrison.info>
2023-07-03 15:35:48 -0400

    What is going on?

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ ORDER BY pulls DESC
 			}
 
 			if ( leaderboard[ pulled_by ] ) {
-				leaderboard.sort((a, b) => a['count'] - b['count']);
+				leaderboard.sort((a, b) => { console.log(a,b); return a['count'] - b['count'] });
 				res.json({ total: leaderboard[ pulled_by ]['count'] });
 			} else {
 				res.json({ total: 0 });
