d938c42a1ca5740318551f713805b2bd093c13f7
Eric Harrison <me@ericharrison.info>
2023-07-05 21:27:47 -0400

    path to mmrs

--- controllers/devleague_admin.js
+++ controllers/devleague_admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { mmrRange, getTierFromMMR } = require('./mmrs');
+const { mmrRange, getTierFromMMR } = require('../mmrs');
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
