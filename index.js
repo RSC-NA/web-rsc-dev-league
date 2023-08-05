4e9d5c93737d9b7a2e6895c7c74cad49cb7fe9a8
Eric Ryan Harrison <me@ericharrison.info>
2023-08-05 14:38:43 -0400

    Adds 2.5.1

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.get('/test', (_req, res) => {
 // THIS WILL BE SET TO true AT RUNTIME, AND IF 
 // THE SERVER EVER CRASHES, IT WILL BE FLIPPED TO FALSE
 let SEND_TO_API_SERVER = true;
-const EXTENSION_VERSION = '2.5.0';
+const EXTENSION_VERSION = '2.5.1';
 const tracker_queue = {};
 
 async function grabMoreTrackers() {
