a6140cf39885e394836127cf5fa2a3b4edc334ed
Eric Harrison <me@ericharrison.info>
2024-03-25 16:03:19 -0400

    use return?

--- controllers/api_devleague.js
+++ controllers/api_devleague.js
@@ -129,7 +129,7 @@ router.all('/check_in', (req, res) => {
 
 						req.session.checked_in = true;
 						console.log('Check in complete -', discord_id, results[0].name);
-						res.status(200).json({ 'success': 'You are checked in!' });
+						return res.json({ 'success': 'You are checked in!' });
 					}
 				);
 			}
