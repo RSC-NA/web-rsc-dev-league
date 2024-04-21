f1273e09bb2c9e7d999c86127c2028464008ce25
Eric Harrison <me@ericharrison.info>
2024-04-20 20:05:55 -0400

    Calc win_pct

--- controllers/api_combines.js
+++ controllers/api_combines.js
@@ -233,7 +233,7 @@ router.get('/games', async(req,res) => {
 	const players_query = `
 		SELECT
 			season,rsc_id,discord_id,name,tier,wins,losses,
-			wins + losses AS games,wins / (wins+losses) as win_pct
+			wins + losses AS games,((wins / (wins+losses)) *100) as win_pct
 		FROM tiermaker 
 		WHERE season = ? AND (losses > 0 OR wins > 0)
 		ORDER BY rsc_id ASC  
@@ -246,7 +246,7 @@ router.get('/games/:rsc_id_or_discord_id', async(req,res) => {
 	const players_query = `
 		SELECT
 			season,rsc_id,discord_id,name,tier,wins,losses,
-			wins + losses AS games,wins / (wins+losses) as win_pct
+			wins + losses AS games,((wins / (wins+losses)) * 100) as win_pct
 		FROM tiermaker 
 		WHERE season = ? AND (rsc_id = ? OR discord_id = ?) AND (losses > 0 OR wins > 0)
 		ORDER BY rsc_id ASC  
