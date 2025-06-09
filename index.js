449b48bf26ed909ca31073ef5885b202b300bf06
Eric Harrison <me@ericharrison.info>
2025-06-09 15:08:34 -0400

    Bump version

--- index.js
+++ index.js
@@ -1339,7 +1339,7 @@ app.get('/numbers/:date', (req, res) => {
 // THIS WILL BE SET TO true AT RUNTIME, AND IF 
 // THE SERVER EVER CRASHES, IT WILL BE FLIPPED TO FALSE
 let SEND_TO_API_SERVER = true;
-const EXTENSION_VERSION = '3.2.3';
+const EXTENSION_VERSION = '3.2.4';
 const tracker_queue = {};
 
 async function grabMoreTrackers() {
