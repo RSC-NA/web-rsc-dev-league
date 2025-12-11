5bc922fb44b0ee3ca20ff4d1fecd7a8d4c86536c
Eric Harrison <me@ericharrison.info>
2025-12-10 20:33:53 -0500

    Bump extension version

--- index.js
+++ index.js
@@ -1383,7 +1383,7 @@ app.get('/numbers/:date', (req, res) => {
 // THIS WILL BE SET TO true AT RUNTIME, AND IF 
 // THE SERVER EVER CRASHES, IT WILL BE FLIPPED TO FALSE
 let SEND_TO_API_SERVER = true;
-const EXTENSION_VERSION = '3.2.5';
+const EXTENSION_VERSION = '3.2.6';
 const tracker_queue = {};
 
 async function grabMoreTrackers() {
