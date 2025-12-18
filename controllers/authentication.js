1f62c8a1c7162cf775950a92189195048a93d7df
Eric Harrison <me@ericharrison.info>
2025-12-17 19:37:07 -0500

    Remove test code

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -128,10 +128,9 @@ router.get('/oauth2', async (req, res) => {
 				ON p.discord_id = t.discord_id
 				WHERE p.discord_id = ?
 			`;
-			if ( discord_id === '207266416355835904' ) {
-				// discord_id = '864178831594356786';
-				discord_id = '444636429369212969';
-			}
+			// if ( discord_id === '207266416355835904' ) {
+				// discord_id = '444636429369212969'; // gama
+			// }
 			req.db.query(
 				query, 
 				[ discord_id ],
