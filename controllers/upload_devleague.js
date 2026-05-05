eb015eb3934b08871709cac565cf4ae836e6792d
Eric Harrison <me@ericharrison.info>
2026-05-04 22:21:36 -0400

    Allow for uploads on MD 0

--- controllers/upload_devleague.js
+++ controllers/upload_devleague.js
@@ -5,7 +5,8 @@ const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		const season = parseInt(req.params.season) ?? null;
 		const day = parseInt(req.params.match_day) ?? null;
-		if ( season && day ) {
+		console.log('upload processing - ', req.params.season, req.params.match_day, season, day);
+		if ( season !== null && day !== null ) {
 			const replay_path = `static/devleague_replays/s${season}/md${day}`;
 			if (!fs.existsSync(replay_path)){
 				fs.mkdirSync(replay_path, { recursive: true });
