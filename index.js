8e7d041b801c19a182fe022e3263cbd018d3ad92
Eric Harrison <me@ericharrison.info>
2026-09-02 17:49:54 -0400

    Implements tier mmr display on dashboard

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const API_HOST = 'api.rscna.com';
 // TODO(erh): this can probably be moved into a database table
 const matchDays = require('./matchDays');
 const combineDays = require('./combineDays');
-const { mmrRange_3s, mmrRange_dev, mmrRange_2s, getTierFromMMR } = require('./mmrs');
+const { mmrRange_3s, mmrRange_dev, mmrRange_2s, getTierFromDevMMR, getTierFromMMR } = require('./mmrs');
 const { roles } = require('./roles');
 
 function writeError(error) {
@@ -897,6 +897,7 @@ app.get('/', (req, res) => {
 		res.render('dashboard', { 
 			match_days: matchDays, //res.locals.match_days,
 			roles: roles,
+			getTierFromMMR: getTierFromMMR,
 			user_roles: req.session?.user_roles ?? null,
 			dashboard_error: req.query?.error ?? false,
 		});
